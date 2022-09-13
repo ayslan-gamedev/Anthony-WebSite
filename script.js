7941458bec4b8d9d9005aa395789e5642e0c8bb1
Ayslan Gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2022-09-13 10:11:06 -0300

    no message

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-console.log(document.querySelector('h1'))
+const themeSwitch = document.querySelectorAll('.theme-switch')
 
-// const $html = document.querySelector('html')
-// const $checkbox = document.querySelector('change', function(){
-//     $html.classList.toggle('Dark Mode')
-// })
+function toggleTheme(e){
+    const body = document.querySelector("body")
+    const theme = this.value
+
+    body.classList.remove("theme-light", "theme-dark")
+    body.classList.add(`theme-${theme}`)
+}
+
+Array.from(themeSwitch).forEach( radio => radio.addEventListener('change', toggleTheme) )
