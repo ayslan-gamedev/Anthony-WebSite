9552bb0e570df2607ca79419d2788da66d7fa2c6
Ayslan Gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2022-09-13 09:21:48 -0300

    Script

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log(document.querySelector('h1'))
+
+// const $html = document.querySelector('html')
+// const $checkbox = document.querySelector('change', function(){
+//     $html.classList.toggle('Dark Mode')
+// })
