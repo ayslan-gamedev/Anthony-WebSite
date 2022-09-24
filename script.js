e873c850534fa9b6e3a94448e051b039bf7552a8
Ayslan Gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2022-09-24 15:05:28 -0300

    no message

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-const themeSwitch = document.querySelectorAll('.theme-switch')
+const themeSwitch = document.querySelector('.theme-switch')
 
-function toggleTheme(e){
-    const body = document.querySelector("body")
-    const theme = this.value
-
-    body.classList.remove("theme-light", "theme-dark")
-    body.classList.add(`theme-${theme}`)
-}
-
-Array.from(themeSwitch).forEach( radio => radio.addEventListener('change', toggleTheme) )
+themeSwitch.addEventListener('change',function(){
+  document.querySelector("body").classList.toggle("theme-dark")
+})
